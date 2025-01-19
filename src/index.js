c793cd99bbe2851b2854caf74395d3d689eacbdf
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-19 12:13:19 -0500

    Write variables to store need weather data

--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ const getWeather = async (location) => {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=TQUULW4UST7ES7XPHWB4N2T5F`;
     try {
         const response = await fetch(url, {mode: 'cors'});
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
         const weatherData = await response.json();
+        const currentDescription = weatherData.currentConditions.conditions;
+        const minTemp = weatherData.days[0].tempmin;
+        const maxTemp = weatherData.days[0].tempmax;
+        const todayDescription = weatherData.days[0].conditions;
         console.log(weatherData);
+        console.log(currentDescription, minTemp, maxTemp, todayDescription);
     } catch(e) {
-        alert(e);
+        console.error(e.message);
     }
     
 }
