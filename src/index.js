72a68c4109240a3f41e34578039ef9834a85d4ba
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-19 11:55:12 -0500

    Write initial getWeather function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello This is a Javascript project.");
+import './styles/comeau-reset.css';
+import './styles/styles.css';
+
+
+const getWeather = async (location) => {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=TQUULW4UST7ES7XPHWB4N2T5F`;
+    try {
+        const response = await fetch(url, {mode: 'cors'});
+        const weatherData = await response.json();
+        console.log(weatherData);
+    } catch(e) {
+        alert(e);
+    }
+    
+}
+
+getWeather(45231);
